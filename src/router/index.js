486c1e3955612d91148b2dfdca40500df18980c2
saddam <saddam@vaultspay.ae>
2023-07-17 04:25:31 +0500

    Update Router and stores

--- src/router/index.js
+++ src/router/index.js
@@ -1,10 +1,12 @@
 import { createRouter, createWebHistory } from 'vue-router'
 import authRoutes from '../modules/Auth/routes'
-
+import productRoutes from '../modules/Product/routes'
+import cartRoutes from '../modules/Cart/routes'
 
 const routes =  [
     ...authRoutes,
-   
+    ...productRoutes,
+    ...cartRoutes
 ]
 
 let router = createRouter({
--- src/store/index.js
+++ src/store/index.js
@@ -1,13 +1,17 @@
 import { createStore } from 'vuex'
 import auth from "../modules/Auth/store"
-
-
-
+import product from "../modules/Product/store"
+import cart from "../modules/Cart/store"
+import common from "../modules/Common/store"
+import analytics from "../modules/Analytics/store"
 
 const store = createStore({
   modules: {
     auth,
- 
+    product,
+    cart,
+    common,
+    analytics
   },
 });
 
